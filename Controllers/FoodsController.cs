8fd1d8bdd25d586154d95d5de1b1b722bca0ebe5
rmkira <rmkira00125@gmail.com>
2021-02-17 19:29:12 +0800

    website

---
+++ Controllers/FoodsController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using FoodOrder.Models;
+
+namespace FoodOrder.Controllers
+{
+    public class FoodsController : Controller
+    {
+        public IActionResult Random()
+        {
+            var food = new Food() { Name = "Fries" };
+            return View(food);
+        }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using FoodOrder.Models;
+
+namespace FoodOrder.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Cart()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Signup()
+        {
+            ViewData["Message"] = "Your Signup Page.";
+
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            ViewData["Message"] = "Your Signup Page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
